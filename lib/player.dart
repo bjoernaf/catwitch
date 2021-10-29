afe7175d6d470c61f142b354012638456eb56034
Bjoern Forsberg <bjoernf@iis.ee.ethz.ch>
2021-10-30 00:50:41 +0200

    Added ugly witch image and falling witch

--- lib/player.dart
+++ lib/player.dart
@@ -3,9 +3,19 @@ import 'package:flame/game.dart';
 import 'package:flame/components.dart';
 
 class Player extends SpriteComponent {
+
+  var falling = true;
+
   @override
   Future<void> onLoad() async {
     await super.onLoad();
-    sprite = await Sprite.load("player.png");
+    sprite = await Sprite.load("witch.png");
+  }
+
+  @override
+  void update(double dt) {
+    if (falling) {
+      position.y += 200.0 * dt;
+    }
   }
 }
